8a07ea2b0df65c68a242f90a35d11ff7647b85d9
IvanKayukoff <IvanKayukoff@gmail.com>
2019-06-01 23:22:09 +0300

    add small bootstrap script

---
+++ install-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mv ~/.zshrc ~/.zshrc.old
+ln -s `pwd`/.zshrc ~/.zshrc
+
+mv ~/.vimrc ~/.vimrc.old
+ln -s `pwd`/.vimrc ~/.vimrc
+
+mkdir -p ~/.config
+mv ~/.config/i3 ~/.config/i3-old
+ln -s `pwd`/i3 ~/.config
+
+mv ~/.config/i3status ~/.config/i3status-old
+ln -s `pwd`/i3status ~/.config
+
+mv ~/.config/terminator ~/.config/terminator-old
+ln -s `pwd`/terminator ~/.config
+
+mv ~/.config/awesome ~/.config/awesome-old
+ln -s `pwd`/awesome ~/.config
+
--- system-bootstrap.sh
+++ system-bootstrap.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-# TODO install essentials, oh-my-zsh
+echo "First of all create your own user who is groupmember of \"wheel\"."
+echo "Next you have to uncomment line with text below to allow members of"
+echo "group \"wheel\" to execute any command:"
+echo '%wheel ALL=(ALL) ALL'
+echo "In the \"/etc/sudoers\" file."
+echo "Type \"yes\" if you've already done that:"
+# Here should be check
 
-# creating symlinks to .zshrc and .vimrc
-mv ~/.zshrc ~/.zshrc.old
-ln -s `pwd`/.zshrc ~/.zshrc
+# Check that runner isn't root
 
-mv ~/.vimrc ~/.vimrc.old
-ln -s `pwd`/.vimrc ~/.vimrc
+sudo pacman --noconfirm -Syu
+sudo pacman --noconfirm -S alsa-utils pulseaudio pavucontrol
+sudo pacman --noconfirm -S xorg-server xorg-xinit mesa
 
-mkdir -p ~/.config
-mv ~/.config/i3 ~/.config/i3-old
-ln -s `pwd`/i3 ~/.config
+sudo pacman --noconfirm -S vim htop git wget curl zsh make cmake terminator openssh networkmanager network-manager-applet
+sudo systemctl enable NetworkManager
 
-mv ~/.config/i3status ~/.config/i3status-old
-ln -s `pwd`/i3status ~/.config
+sudo pacman --noconfirm -S powerline-fonts ttf-ubuntu-font-family tamsyn-font ttf-droid ttf-dejavu ttf-fira-code ttf-fira-sans
 
-mv ~/.config/terminator ~/.config/terminator-old
-ln -s `pwd`/terminator ~/.config
+sudo pacman --noconfirm -S awesome gdm compton xxkb firefox
+sudo systemctl enable gdm
 
-mv ~/.config/awesome ~/.config/awesome-old
-ln -s `pwd`/awesome ~/.config
+echo "Now we're going to install oh-my-zsh, so don't forget to press Ctrl+D when it'll be installed"
+echo "Type \"yes\" if you agree:"
+# Here should be check
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#sudo ln -s `pwd`/asgard_backup_push.sh /usr/local/bin/asgard_backup_push
+# Pull dotfiles submodules
+git submodule update --recursive
+sh install-dotfiles.sh
+
+# Installing yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+echo "Bootstrap completed. Now you can reboot and run the next script" # Add the next script name
 
