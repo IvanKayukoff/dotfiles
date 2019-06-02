07bab730747588bf812c9f0e5f5b009da935ed4b
IvanKayukoff <IvanKayukoff@gmail.com>
2019-06-02 18:19:34 +0300

    install gtk theme and icons

--- system-bootstrap.sh
+++ system-bootstrap.sh
@@ -11,15 +11,16 @@ echo "Type \"yes\" if you've already done that:"
 # Check that runner isn't root
 
 sudo pacman --noconfirm -Syu
-sudo pacman --noconfirm -S alsa-utils pulseaudio pavucontrol
-sudo pacman --noconfirm -S xorg-server xorg-xinit mesa xorg-xinput
+sudo pacman --noconfirm -S alsa-utils pulseaudio pavucontrol xorg-server \
+  xorg-xinit mesa xorg-xinput htop git wget curl zsh make cmake terminator \
+  openssh networkmanager network-manager-applet screen screenfetch \
+  powerline-fonts ttf-ubuntu-font-family tamsyn-font ttf-droid ttf-dejavu \
+  ttf-fira-code ttf-fira-sans awesome gdm compton xxkb firefox ranger \
+  atool lynx ffmpegthumbnailer highlight mediainfo poppler python-chardet w3m \
+  python-pip rxvt-unicode rxvt-unicode-terminfo xsel xorg-xfontsel gvim vlc \
+  strace nautilus acpid playerctl cmus
 
-sudo pacman --noconfirm -S vim htop git wget curl zsh make cmake terminator openssh networkmanager network-manager-applet
 sudo systemctl enable NetworkManager
-
-sudo pacman --noconfirm -S powerline-fonts ttf-ubuntu-font-family tamsyn-font ttf-droid ttf-dejavu ttf-fira-code ttf-fira-sans
-
-sudo pacman --noconfirm -S awesome gdm compton xxkb firefox
 sudo systemctl enable gdm
 
 echo "Now we're going to install oh-my-zsh, so don't forget to press Ctrl+D when it'll be installed"
@@ -28,7 +29,7 @@ echo "Type \"yes\" if you agree:"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Pull dotfiles submodules
-git submodule update --recursive
+git submodule update --init --recursive
 sh install-dotfiles.sh
 
 # Installing yay
@@ -38,5 +39,26 @@ makepkg -si
 cd ..
 rm -rf yay
 
-echo "Bootstrap completed. Now you can reboot and run the next script" # Add the next script name
+yay --noconfirm -S urxvt-perls gtk-theme-shades-of-gray
+
+echo "set preview_images true"        > ~/.config/ranger/rc.conf
+echo "set preview_images_method w3m" >> ~/.config/ranger/rc.conf
+
+# Installing icons
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git
+cd Qogir-icon-theme
+sh Install
+cd ..
+rm -rf Qogir-icon-theme
+
+# Setting the gtk theme and icons
+mkdir -p ~/.config/gtk-3.0
+GTK3_SETTINGS='~/.config/gtk-3.0/settings.ini'
+echo '[Settings]'                            > $GTK3_SETTINGS
+echo 'gtk-icon-theme-name = Qogir-dark'     >> $GTK3_SETTINGS
+echo 'gtk-theme-name = Shades-of-gray-Arch' >> $GTK3_SETTINGS
+echo 'gtk-font-name = Cantarell 11'         >> $GTK3_SETTINGS
+
+echo "Bootstrap completed. Now you can reboot and run the next script"
+# TODO Add the next script name
 
