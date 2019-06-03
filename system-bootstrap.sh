14cc4a580d71e6e2797601fca2ecd4b50f504296
IvanKayukoff <IvanKayukoff@gmail.com>
2019-06-03 13:51:39 +0300

    add necessary checks to bootstrap script

--- system-bootstrap.sh
+++ system-bootstrap.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-echo "First of all create your own user who is groupmember of \"wheel\"."
-echo "Next you have to uncomment line with text below to allow members of"
-echo "group \"wheel\" to execute any command:"
-echo '%wheel ALL=(ALL) ALL'
-echo "In the \"/etc/sudoers\" file."
-echo "Type \"yes\" if you've already done that:"
-# Here should be check
+PATH_OF_ZSHRC=`find . -type f -name .zshrc`
 
-# Check that runner isn't root
+if [[ $PATH_OF_ZSHRC != './.zshrc' ]]
+then
+  echo "Working directory must be is the root of dotfiles"
+  exit 1
+fi
 
+if [[ `whoami` = 'root' ]]
+then
+  echo "To run this script you can't be logged in as root."
+  echo "First of all create your own user who is groupmember of \"wheel\"."
+  echo "Also, you have to permit members of wheel to execute any command."
+  echo "You can do it under root via:"
+  echo "sed -i 's/.*\(%wheel *ALL *= *( *ALL *) *ALL.*\)/\1/' /etc/sudoers"
+  exit 1
+fi
+
+sudo true
+IS_SUDO_FAILED=$?
+if [[ $IS_SUDO_FAILED != '0' ]]
+then
+  echo "\"$USER\" must be a member of group \"wheel\""
+  exit 1
+fi
+
+# Awesome brightness widget requires availability to run as root without password
+echo "ALL ALL = (root) NOPASSWD: `pwd`/awesome/widgets/brightness/set_brightness.sh"\
+  | sudo tee --append /etc/sudoers
+
+# System update
 sudo pacman --noconfirm -Syu
+
+# Install essential programs and utilities
 sudo pacman --noconfirm -S alsa-utils pulseaudio pavucontrol xorg-server \
   xorg-xinit mesa xorg-xinput htop git wget curl zsh make cmake terminator \
   openssh networkmanager network-manager-applet screen screenfetch \
@@ -24,8 +47,8 @@ sudo systemctl enable NetworkManager
 sudo systemctl enable gdm
 
 echo "Now we're going to install oh-my-zsh, so don't forget to press Ctrl+D when it'll be installed"
-echo "Type \"yes\" if you agree:"
-# Here should be check
+echo "Press \"Enter\" to continue.. "
+read
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Pull dotfiles submodules
@@ -41,6 +64,7 @@ rm -rf yay
 
 yay --noconfirm -S urxvt-perls gtk-theme-shades-of-gray
 
+mkdir -p ~/.config/ranger
 echo "set preview_images true"        > ~/.config/ranger/rc.conf
 echo "set preview_images_method w3m" >> ~/.config/ranger/rc.conf
 
@@ -59,6 +83,5 @@ echo 'gtk-icon-theme-name = Qogir-dark'     >> $GTK3_SETTINGS
 echo 'gtk-theme-name = Shades-of-gray-Arch' >> $GTK3_SETTINGS
 echo 'gtk-font-name = Cantarell 11'         >> $GTK3_SETTINGS
 
-echo "Bootstrap completed. Now you can reboot and run the next script"
-# TODO Add the next script name
+echo "Bootstrap completed! Now you can reboot to start using awesome. Enjoy it ;)"
 
