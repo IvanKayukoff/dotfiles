036430725fc2ccd237f5258183ec13e798424c12
Vanchesky <ivankayukoff@gmail.com>
2019-09-15 22:21:14 +0300

    refactor install-dotfiles.sh script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-mv ~/.zshrc ~/.zshrc.old 2>/dev/null
-ln -s `pwd`/.zshrc ~/.zshrc
+# Creates soft link to the $dotfile_name and places it to $desired_path.
+# Also, the function saves old version of a file that located in $desired_path.
+# $dotfile_name is a relative path to file/directory starting from 
+#               the dotfile repository root.
+# $desired_path is a absolute path to the desired location of the new symlink.
+new_safe_symlink () {
+  local dotfile_name="$1"
+  local desired_path="$2"
+  mv "$desired_path" "$desired_path.old" 2>/dev/null
+  ln -s "`pwd`/$dotfile_name" "$desired_path"
+}
 
-mv ~/.vimrc ~/.vimrc.old 2>/dev/null
-ln -s `pwd`/.vimrc ~/.vimrc
+new_safe_symlink ".zshrc"       "$HOME/.zshrc"
+new_safe_symlink ".vimrc"       "$HOME/.vimrc"
 
 mkdir -p ~/.config
-mv ~/.config/i3 ~/.config/i3-old 2>/dev/null
-ln -s `pwd`/i3 ~/.config
 
-mv ~/.config/i3status ~/.config/i3status-old 2>/dev/null
-ln -s `pwd`/i3status ~/.config
-
-mv ~/.config/terminator ~/.config/terminator-old 2>/dev/null
-ln -s `pwd`/terminator ~/.config
-
-mv ~/.config/awesome ~/.config/awesome-old 2>/dev/null
-ln -s `pwd`/awesome ~/.config
-
-mv ~/.config/compton.conf ~/.config/compton.conf.old 2>/dev/null
-ln -s `pwd`/compton.conf ~/.config
-
-mv ~/.xprofile ~/.xprofile.old 2>/dev/null
-ln -s `pwd`/.xprofile ~/.xprofile
-
-mv ~/.Xresources ~/.Xresources.old 2>/dev/null
-ln -s `pwd`/.Xresources ~/.Xresources
+new_safe_symlink "i3"           "$HOME/.config/i3"
+new_safe_symlink "i3status"     "$HOME/.config/i3status"
+new_safe_symlink "terminator"   "$HOME/.config/terminator"
+new_safe_symlink "awesome"      "$HOME/.config/awesome"
+new_safe_symlink ".xprofile"    "$HOME/.xprofile"
+new_safe_symlink ".Xresources"  "$HOME/.Xresources"
 
